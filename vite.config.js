1f86827831ec611f56c29a23c812d7fccc8d22c8
ralfa17 <ridhoalfattaah17@gmail.com>
2026-09-15 13:49:48 +0700

    ubah logo bawaan jadi logo benang merah

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -16,13 +16,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
+            src: '/bm.png',
+            sizes: '192x192 512x512',
             type: 'image/png'
           }
         ]
